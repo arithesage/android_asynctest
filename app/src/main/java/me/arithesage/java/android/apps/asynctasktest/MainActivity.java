021c52c97e0a22eee74cf5652e429be89d94703b
arithesage <ariaththesage@gmail.com>
2024-06-20 10:22:40 +0200

    Initial upload.

---
+++ app/src/main/java/me/arithesage/java/android/apps/asynctasktest/MainActivity.java
@@ -0,0 +1,14 @@
+package me.arithesage.java.android.apps.asynctasktest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
