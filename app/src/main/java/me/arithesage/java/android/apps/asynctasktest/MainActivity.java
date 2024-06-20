a45d31233451949b1e5dc5c7cdae3c1cce397c2c
arithesage <ariaththesage@gmail.com>
2024-06-20 12:35:32 +0200

    At first, this seems to work. Need to test with network code.

--- app/src/main/java/me/arithesage/java/android/apps/asynctasktest/MainActivity.java
+++ app/src/main/java/me/arithesage/java/android/apps/asynctasktest/MainActivity.java
@@ -3,12 +3,22 @@ package me.arithesage.java.android.apps.asynctasktest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import me.arithesage.java.android.libs.helpers.OSHelpers;
+import me.arithesage.java.android.libs.helpers.OSHelpers.AndroidAsyncTask;
+
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        OSHelpers.RunAsync (new AndroidAsyncTask(() ->
+        {
+            String greeting = "Hello from async task.";
+            Log.d ("DUMMY", greeting);
+        }));
     }
 }
